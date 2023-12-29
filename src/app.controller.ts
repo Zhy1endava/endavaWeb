a56be5afceb3ee3e3f834ac07f74150ed9768681
zhy1endava <zivan.sinzar@endava.com>
2023-12-29 13:55:30 +0100

    AdSense attempt1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,4 @@
-import {Controller, Get, Redirect, Res} from '@nestjs/common';
-import {Response} from 'express';
-import {join} from 'path';
+import {Controller, Get, Redirect} from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -9,45 +7,5 @@ export class AppController {
     @Redirect('/public', 302) // This redirects to the /public route
     redirectToPublic() {
         // This method will redirect from the base URL to '/public'
-    }
-
-    @Get('ad01/') // Handle the /ad01/ URL
-    getAd01(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'ad01.html'));
-    }
-
-    @Get('ad02/') // Handle the /ad02/ URL
-    getAd02(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'ad02.html'));
-    }
-
-    @Get('ad03/') // Handle the /ad03/ URL
-    getAd03(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'ad03.html'));
-    }
-
-    @Get('ad04/') // Handle the /ad04/ URL
-    getAd04(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'ad04.html'));
-    }
-
-    @Get('kw_ad1/') // Handle the /ad01/ URL
-    getKwAd01(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'kw_ad1.html'));
-    }
-
-    @Get('kw_ad2/') // Handle the /ad02/ URL
-    getKwAd02(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'kw_ad2.html'));
-    }
-
-    @Get('kw_ad3/') // Handle the /ad03/ URL
-    getKwAd03(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'kw_ad3.html'));
-    }
-
-    @Get('kw_ad4/') // Handle the /ad04/ URL
-    getKwAd04(@Res() res: Response) {
-        return res.sendFile(join(__dirname, '..', 'public', 'kw_ad4.html'));
     }
 }
